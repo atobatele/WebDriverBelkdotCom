e9c5b82aa4806719d50e3bd6449ed62b3d616b0b
Kayode Atobatele <katobatele@fuzzproductions.com>
2014-09-30 14:09:32 -0400

    first commit

---
+++ src/main/java/com/fuzzpro/webdriver/App.java
@@ -0,0 +1,13 @@
+package com.fuzzpro.webdriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
